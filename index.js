d48c4da398b7dc7790d2865651d683a6e3d6278e
Brandon_Lopez <brandonchristopherlopez@gmail.com>
2024-04-24 11:50:17 -0700

    adding more and more functions from module 12 for root index

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function startApp() {
     promptUser();
 }
 
-  // View all employees
+  // View all employees**** needed help on this one 
 function viewEmployees() {
     db.findAllEmployees()
       .then(([rows]) => {
@@ -23,8 +23,7 @@ function viewEmployees() {
         console.table(employees);
       })
       .then(() => loadMainPrompts());
-  }
-  
+}
 
   // Function to display all employees
 function displayAllEmployees() {
@@ -72,3 +71,80 @@ function promptAndDisplayEmployeesByDepartment() {
     })
     .then(() => promptUserForNextAction()); 
 }
+// View all employees reporting tro a specific manager 
+function viewEmployeesReportingToManager() {
+    // Fetches all employees from  DB
+    db.getAllEmployees()
+        .then((result) => {
+            const managers = result[0];
+
+            // Prompt the list of managers
+            const managerOptions = managers.map(manager => ({
+                name: `${manager.first_name} ${manager.last_name}`,
+                value: manager.id
+            }));
+
+            // Allow user to select a manager
+            return prompt([
+                {
+                    type: "list",
+                    name: "selectedManager",
+                    message: "Select a manager to see direct reports:",
+                    choices: managerOptions
+                }
+            ]);
+        })
+        .then((response) => {
+            // Find reports of the selected manager
+            return db.getEmployeesByManager(response.selectedManager);
+        })
+        .then((result) => {
+            const employees = result[0];
+
+            // Display the employees who report to the selected manager
+            console.log("\n");
+            if (employees.length === 0) {
+                console.log("The selected manager has no direct reports.");
+            } else {
+                console.table(employees);
+            }
+        })
+        .then(() => promptUserForNextAction());
+}
+// Remove employee from DB
+function deleteEmployee() {
+    // Find all employees from db
+    db.getAllEmployees()
+        .then((result) => {
+            const employees = result[0];
+
+            // Prompt users with employee selections
+            const employeeOptions = employees.map(employee => ({
+                name: `${employee.first_name} ${employee.last_name}`,
+                value: employee.id
+            }));
+
+            // Select a employee to remove
+            return prompt([
+                {
+                    type: "list",
+                    name: "selectedEmployeeId",
+                    message: "Select an employee to remove from the database:",
+                    choices: employeeOptions
+                }
+            ]);
+        })
+        .then((response) => {
+            const employeeId = response.selectedEmployeeId;
+
+            // REMOVED!
+            return db.deleteEmployeeById(employeeId);
+        })
+        .then(() => {
+            console.log("Employee has been successfully removed from the database.");
+        })
+        .then(() => promptUserForNextAction()); 
+}
+
+//NEXT
+
